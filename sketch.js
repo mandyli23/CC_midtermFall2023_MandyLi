1c6b2fcf79ec900eceb0b6dd13496e6e6577e253
manli167 <ml8836@nyu.edu>
2023-10-19 20:13:33 -0400

    first push

--- sketch.js
+++ sketch.js
@@ -1,14 +1,28 @@
 
 function setup() {
   createCanvas(800, 800);
-  
 }
 
 function draw() {
-  
-
-
-}
+  if (option == 1) {    //option 1
+    background(130, 225, 40);
+      noStroke();
+      ellipse(width/2, height/2, 700 , 700);
+        for(x = mouseX; x <= 700 && x >= 100; x++ ){
+        oneDisease();
+          if(x == 700 || x == 100){
+          mouseX + oneDisease();  //probably need to change this from func to OOP in order to duplicate
+              
+          }//if
+          
+    background(0, 0, 255);
+ function oneDisease(){  
+  map(mouseX, 0, 10, 0, 800);
+    option++;
+    if( option >= 4) {
+      option = 1;
+    }
+  }
 
 
   
