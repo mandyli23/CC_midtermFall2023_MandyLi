adbb92f8aeacff4162adfffe86444b739d41dbf2
manli167 <ml8836@nyu.edu>
2023-10-27 17:22:59 -0400

    third push

--- BallOne.js
+++ BallOne.js
@@ -1,16 +1,17 @@
 class Ball {
-    constructor(newX, newY) {
+    constructor(newX, newY) {   //x & y location parameters
       this.x = newX;
       this.y = newY;
-      this.r = 25;
-      this.xSpeed = random(-2, 2);
+      this.r = 25;   //radius
+      this.xSpeed = random(-2, 2);  //random speeds 
       this.ySpeed = random(-2, 2);
     }
   
     move() {
-      this.x += this.xSpeed;
+      this.x += this.xSpeed; //move balls
       this.y += this.ySpeed;
   
+      //checks edges
       if (this.x < 0 || this.x > width) {
         this.xSpeed *= -1;
       }
@@ -21,12 +22,13 @@ class Ball {
   
     display() {
       fill(0, 255, 0);
+      noStroke();
       ellipse(this.x, this.y, this.r * 2);
     }
   
     intersects(other) {
-      let d = dist(this.x, this.y, other.x, other.y);
-      return d < this.r + other.r;
+      let d = dist(this.x, this.y, other.x, other.y); //checking distance between two circles and their radii
+      return d < this.r + other.r; //boolean to check distance
     }
   
     
--- sketch.js
+++ sketch.js
@@ -6,9 +6,12 @@ function setup() {
   createCanvas(800, 800);
 }
 
-function draw() {
-  background(130, 225, 40);
+let option = 0;
 
+function draw() {
+  background(255, 225, 255);
+
+  if(option === 1){
   for (let i = 0; i < balls.length; i++) {
     balls[i].move();
     balls[i].display();
@@ -33,8 +36,58 @@ function draw() {
   }
 }
 
+if (option === 2){
+  //scene2
+  background(255, 225, 255);
+  //scale(random(1, 3));
+  myCellCluster(0);
+  myCellCluster(200);
+  myCellCluster(400);
+  myCellCluster(600);
+  myCellCluster(800);
+}
 
+}
+
+if (option === 2){
+function keyPressed() {
+  noLoop();
+}
+
+function keyReleased() {
+  loop();
+}
+}
+
+if (option === 1){
 function mousePressed() {
   balls.push(new Ball(mouseX, mouseY));
 }
+}
 
+function myCell(x1, y1, a1, a2) { //this function is for one blinking cell
+  noStroke();
+  //fill(random(y/2,0, x/4));
+  fill((x1-100)/1.5,0, 0);
+  ellipse (x1, y1, a1, a2);
+  
+}
+
+function myCellCluster(y){
+  myCell(0, y,random(100),random(300));
+  myCell(400, y,random(100),random(300));
+  myCell(300, y,random(100),random(300));
+  myCell(200, y,random(100),random(300));
+  myCell(100, y,random(100),random(300));
+  myCell(500, y,random(100),random(300));
+  myCell(600, y,random(100),random(300));
+  myCell(700, y,random(100),random(300));
+  myCell(800, y,random(100),random(300));
+}
+
+function mousePressed(){
+  option++;
+  if(option > 3) {
+    option = 1;
+  }
+}
